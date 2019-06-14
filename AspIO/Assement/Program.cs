fd058a1826b5ddf4c2180e7af03330dfa86ac749
Chiziii <igwechizy@gmail.com>
2019-06-14 17:41:10 +0100

    This is the commit

---
+++ AspIO/Assement/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assement
+{
+    class Program
+    {
+        //The path to the text file, where all data is saved
+         string filepath = "C:\\Users\\DELL\\Desktop\\viva.txt";
+
+        // Function that Reads out data from the text file
+        public void ReadFromTextfile()
+        {
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        // Function that Saves data to the textfile
+        public void SaveToTextfile()
+        {
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            lines.Add(Console.ReadLine());
+
+
+            File.WriteAllLines(filepath, lines);
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            // instantiate the Program class and call it's functions
+            Program program = new Program();
+            program.ReadFromTextfile();
+            program.SaveToTextfile();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+    /* string filepath = "C:\\Users\\DELL\\Desktop\\viva.txt";
+
+          List<string> lines = File.ReadAllLines(filepath).ToList();
+
+          foreach (string line in lines)
+          {
+              Console.WriteLine(line);
+          }
+
+           /*lines.Add("Name: Chizi" +
+                     "Phone number: 252853526" +
+                     "| Address: 23 Wealth avenue" +
+                     "| Net worth: Forbes is still working on calculating this to include all her assets without leaving any out hbdfhbdhbsh");*/
+
+   /* lines.Add(Console.ReadLine());
+
+
+ File.WriteAllLines(filepath, lines);
+
+ Console.WriteLine();
+
+
+
+ Console.ReadLine(); */
